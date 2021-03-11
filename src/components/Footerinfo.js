b7dd5a6fc0437d0f25ec152b7226835befef2b43
Kay Alvaer <62592167+Kayalvaer@users.noreply.github.com>
2021-03-12 00:48:50 +0100

    remove unused code

--- src/components/Footerinfo.js
+++ src/components/Footerinfo.js
@@ -5,16 +5,7 @@ import Row from 'react-bootstrap/Row';
 function Footerinfo() {
     return (
         <>
-       {/*  <div className="row flex-row text-secondary">
-            <div className="col-md-4 d-flex justify-content-center justify-content-md-start">
-            <a href="https://vimeo.com/" className="e_post text-secondary ml-md-3 my-auto "><i class="fa fa-vimeo" size="lg"></i></a>
-            <a href="https://youtube.com/" className="e_post text-secondary ml-md-3 my-auto"><i className="fa fa-youtube-play"></i></a>
-        </div>
-            <div className="col-md-8 d-flex justify-content-between text-secondary">    
-                <span class="e_post my-auto">hello@yay.com</span>
-                <span class="e_post my-auto">Copyright 2020</span>
-            </div>
-        </div> */}
+     
         <Row className="flex-row text-secondary">
             <div className="col-md-4 d-flex justify-content-center justify-content-md-start">
                 <a href="https://vimeo.com/" className="e_post text-secondary ml-md-3 my-auto "><i class="fa fa-vimeo" size="lg"></i></a>
@@ -22,7 +13,7 @@ function Footerinfo() {
             </div>
             <div className="col-md-8 d-flex justify-content-between text-secondary bg-white">    
                 <span class="e_post my-auto">hello@yay.com</span>
-                <span class="e_post my-auto">Copyright 2020</span>
+                <span class="e_post my-auto">Copyright KayAlvær-2020</span>
             </div>
         </Row>
         </>
